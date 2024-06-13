a0c56edb32c4a069bc82f40e6bfe53b4cd9521a1
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2024-06-13 19:32:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ MAINTAINER_EMAIL = "javierfumanalidocin@gmail.com"
 URL = "https://github.com/Fuminides/ex-fuzzy"
 LICENSE = "GPL-3.0"
 DOWNLOAD_URL = "https://pypi.org/project/ex-fuzzy/"
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 INSTALL_REQUIRES = ["numpy", "networkx", "matplotlib", "pymoo", "pandas", "scikit-learn"]
 CLASSIFIERS = [
     "Development Status :: 4 - Beta",
